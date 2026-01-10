c1c0cbcd9ed7754968c4056bce82d5a8d009b375
Gabrielricco12 <gabrielricco2023@gmail.com>
2026-01-09 22:56:55 -0300

    Refactor App component to include AccountDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './features/auth/AuthContext';
-import { DateProvider } from './features/date/DateContext'; // Importe o Contexto de Data
+import { DateProvider } from './features/date/DateContext';
 import { Toaster } from 'sonner';
 
 // Páginas
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
-import TransactionsPage from './pages/TransactionsPage';
+import TransactionsPage from './pages/TransactionsPage';     // Lista de todas as contas
+import AccountDashboard from './pages/AccountDashboard';     // <--- RECUPERADO: Detalhes de uma conta específica
 import FixedExpensesPage from './pages/FixedExpensesPage';
-import CardsPage from './pages/CardsPage'; // Se tiver criado a página de cartões
+import CardsPage from './pages/CardsPage'; 
 
 // Componente de Rota Privada
 const PrivateRoute = ({ children }) => {
@@ -22,7 +23,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <DateProvider> {/* O DateProvider deve envolver as rotas */}
+        <DateProvider>
           <Toaster position="top-center" richColors />
           <Routes>
             <Route path="/login" element={<LoginPage />} />
@@ -30,7 +31,13 @@ export default function App() {
             
             {/* Rotas Protegidas */}
             <Route path="/" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
+            
+            {/* Página que lista todas as contas e cartões */}
             <Route path="/transactions" element={<PrivateRoute><TransactionsPage /></PrivateRoute>} />
+            
+            {/* <--- ROTA RECUPERADA: Detalhes de uma conta específica (Ex: Nubank) */}
+            <Route path="/accounts/:id" element={<PrivateRoute><AccountDashboard /></PrivateRoute>} />
+            
             <Route path="/fixed-expenses" element={<PrivateRoute><FixedExpensesPage /></PrivateRoute>} />
             <Route path="/cards" element={<PrivateRoute><CardsPage /></PrivateRoute>} />
             
